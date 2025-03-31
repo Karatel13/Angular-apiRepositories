6ccd21fbe5d095da17a7b301ba7442bb514763dd
Karatel13 <davitgogoladze2000@gmail.com>
2025-03-31 13:05:19 +0400

    V1.10001 ReCreate home(cmp)and --> api-card(cmp)

---
+++ src/app/home/api-card/api-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-card',
+  imports: [],
+  templateUrl: './api-card.component.html',
+  styleUrl: './api-card.component.scss'
+})
+export class ApiCardComponent {
+
+}
